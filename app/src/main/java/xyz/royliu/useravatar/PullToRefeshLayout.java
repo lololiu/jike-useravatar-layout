393dff2f06df7f84c1a6a52e603c30b8eed6d287
lololiu <liulou.zh@gmail.com>
2016-12-29 15:05:51 +0800

    Initial commit

---
+++ app/src/main/java/xyz/royliu/useravatar/PullToRefeshLayout.java
@@ -0,0 +1,85 @@
+package xyz.royliu.useravatar;
+
+import android.content.Context;
+import android.util.AttributeSet;
+import android.util.Log;
+import android.view.MotionEvent;
+import android.view.View;
+import android.widget.LinearLayout;
+
+/**
+ * Created by liulou on 2016/12/28.
+ * desc:
+ */
+
+public class PullToRefeshLayout extends LinearLayout {
+    private static final String TAG = "PullToRefeshLayout";
+    private int mHeaderOffsetTop = 0;
+    private int mHeaderOffsetBottom = 0;
+
+    private float mStratY;
+
+    public PullToRefeshLayout(Context context, AttributeSet attrs) {
+        super(context, attrs);
+        mHeaderOffsetTop = ScreenUtil.dip2px(context, 100);
+        mHeaderOffsetBottom = ScreenUtil.dip2px(context, 100);
+    }
+
+    @Override
+    protected void onMeasure(int widthMeasureSpec, int heightMeasureSpec) {
+        int widthMode = MeasureSpec.getMode(widthMeasureSpec);
+        int sizeWidth = MeasureSpec.getSize(widthMeasureSpec);
+        int heightMode = MeasureSpec.getMode(heightMeasureSpec);
+        int sizeHeight = MeasureSpec.getSize(heightMeasureSpec);
+        measureChildren(widthMeasureSpec, heightMeasureSpec);
+        setMeasuredDimension(sizeWidth, sizeHeight);
+    }
+
+    @Override
+    protected void onLayout(boolean changed, int left, int top, int right, int bottom) {
+        int count = getChildCount();
+        if (count != 2) {
+            throw new RuntimeException("should have 2 child views!");
+        }
+        int headHeight = 0;
+        for (int i = 0; i < count; i++) {
+            View childView = getChildAt(i);
+
+            if (i == 0) {
+                headHeight = childView.getMeasuredHeight();
+                childView.layout(0, -mHeaderOffsetTop, childView.getMeasuredWidth(), headHeight - mHeaderOffsetTop);
+            } else if (i == 1) {
+                childView.layout(0, headHeight - mHeaderOffsetTop - mHeaderOffsetBottom, childView.getMeasuredWidth(), headHeight - mHeaderOffsetTop - mHeaderOffsetBottom + childView.getMeasuredHeight());
+            }
+        }
+    }
+
+    @Override
+    public boolean onTouchEvent(MotionEvent event) {
+        switch (event.getAction()) {
+            case MotionEvent.ACTION_DOWN:
+                mStratY = event.getY();
+                Log.d(TAG, "onTouchEvent: start:" + mStratY);
+                return true;
+            case MotionEvent.ACTION_MOVE:
+                float mCurrentY = event.getY();
+                Log.d(TAG, "onTouchEvent: mHeaderOffsetTop-->"+mHeaderOffsetTop);
+                if ((mCurrentY - mStratY) / 2 > mHeaderOffsetTop) {
+                    getChildAt(0).animate().translationY(0);
+                    getChildAt(1).animate().translationY(0);
+                } else {
+                    getChildAt(0).setTranslationY((mCurrentY - mStratY) / 2);
+                    getChildAt(1).setTranslationY((mCurrentY - mStratY));
+                }
+                Log.d(TAG, "onTouchEvent: " + (mCurrentY - mStratY));
+                return true;
+            case MotionEvent.ACTION_UP:
+                getChildAt(0).animate().translationY(0);
+                getChildAt(1).animate().translationY(0);
+                return true;
+            default:
+                break;
+        }
+        return super.onTouchEvent(event);
+    }
+}
---
+++ app/src/main/java/xyz/royliu/useravatar/ScreenUtil.java
@@ -0,0 +1,61 @@
+package xyz.royliu.useravatar;
+
+import android.content.Context;
+import android.util.DisplayMetrics;
+
+/**
+ * Created by roy on 2016/8/9.
+ * desc:
+ */
+public class ScreenUtil {
+    /**
+     * dip转px
+     */
+    public static int dip2px(Context context, float dipValue) {
+        final float scale = context.getResources().getDisplayMetrics().density;
+        return (int) (dipValue * scale + 0.5f);
+    }
+
+    /**
+     * px转dip
+     */
+    public static int px2dip(Context context, float pxValue) {
+        final float scale = context.getResources().getDisplayMetrics().density;
+        return (int) (pxValue / scale + 0.5f);
+    }
+
+    /**
+     * 获取屏幕高度
+     * @param context
+     * @return
+     */
+    public static int getScreenHeight(Context context){
+        if (null == context) {
+            return 0;
+        }
+        DisplayMetrics dm = new DisplayMetrics();
+        dm = context.getApplicationContext().getResources().getDisplayMetrics();
+        return dm.heightPixels;
+    }
+
+    /**
+     * 将px值转换为sp值，保证文字大小不变
+     *            （DisplayMetrics类中属性scaledDensity）
+     * @return
+     */
+    public static int px2sp(Context context, float pxValue) {
+        final float fontScale = context.getResources().getDisplayMetrics().scaledDensity;
+        return (int) (pxValue / fontScale + 0.5f);
+    }
+
+    /**
+     * 将sp值转换为px值，保证文字大小不变
+     *            （DisplayMetrics类中属性scaledDensity）
+     * @return
+     */
+    public static int sp2px(Context context, float spValue) {
+        final float fontScale = context.getResources().getDisplayMetrics().scaledDensity;
+        return (int) (spValue * fontScale + 0.5f);
+    }
+
+}
